b756afbae92a912b9f728aee7bfb93eb09d262bb
utchak160 <chakravartyutkarsh@gmail.com>
2020-06-19 15:44:11 +0530

    my place button added

--- src/components/Places/components/PlaceList/PlaceList.tsx
+++ src/components/Places/components/PlaceList/PlaceList.tsx
@@ -1,16 +1,39 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
+import AddAPhotoRoundedIcon from '@material-ui/icons/AddAPhotoRounded';
 import PlaceItem from "../PlaceItem/PlaceItem";
 import Card from "../../../Shared/Card/Card";
+import Button from "@material-ui/core/Button";
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import './PlaceList.css';
 import {Place} from "../../../../models/place";
 
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        button: {
+            margin: theme.spacing(1),
+        },
+    }),
+);
+
 const PlaceList = (props: any) => {
+    const classes = useStyles();
     if (props.items.length === 0) {
         return (
             <Card>
                 <div className="text-center">
-                    <h3>No items found!</h3>
+                    <h3>No Places found! May be Share one?</h3>
+                    <Link to={`/place/new`}>
+                        <Button
+                            variant="contained"
+                            color="secondary"
+                            className={classes.button}
+                            startIcon={<AddAPhotoRoundedIcon />}
+                        >
+                            Share place
+                        </Button>
+                    </Link>
                 </div>
             </Card>
         );
